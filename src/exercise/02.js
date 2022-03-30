e8d89f797d84951da59abe5f8b92e2de35586b3d
Davinder Singh <davibaweja@gmail.com>
2022-03-30 09:50:05 +0100

    saved file ex-2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,7 +6,9 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(window.localStorage.getItem('name') ?? initialName)
+  const [name, setName] = React.useState(
+    window.localStorage.getItem('name') ?? initialName,
+  )
 
   // 🐨 Here's where you'll use `React.useEffect`.
   React.useEffect(() => {
